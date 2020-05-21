667abee48e35594c41ec438c4719464debbc304a
Egor Surov <surovegor.se@gmail.com>
2020-05-21 18:14:52 +0400

    header version 1.2

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 "use strict";
 
-const searchButton = document.getElementById("search"),
-      form = document.getElementById("form"),
-      closeSearchButton = document.getElementById("close__search"),
-      buttonMenu = document.getElementById("button-menu"),
-      navCover = document.getElementById("mobile");
+const searchButton = document.querySelector(".search__icon"),
+      form = document.querySelector(".form"),
+      buttonMenu = document.querySelector(".button-menu"),
+      mobile = document.querySelector(".header__right"),
+      closeSearchButton = document.getElementById("close__search");
 
-    searchButton.addEventListener("click", () => { 
+  function openForm() {
+      form.classList.remove("open");
+      searchButton.style.display = "block";
+      closeSearchButton.style.display = "none";
+    }
+
+  searchButton.addEventListener("click", () => { 
       form.classList.add("open");
       searchButton.style.display = "none";
       closeSearchButton.style.display = "block";
     });
    
-    document.addEventListener('click', (e) => {
-      if (!form.contains(e.target) && !searchButton.contains(e.target)) {
-        form.classList.remove("open");
-         searchButton.style.display = "block";
-         closeSearchButton.style.display = "none";
-      }
-    });
-    
-    closeSearchButton.addEventListener("click", () => {
-      form.classList.remove("open");
-        searchButton.style.display = "block";
-        closeSearchButton.style.display = "none";
+  document.addEventListener('click', (e) => !form.contains(e.target) && !searchButton.contains(e.target) ?  openForm() : false)
+
+  closeSearchButton.addEventListener("click", () => openForm())
+
+  buttonMenu.addEventListener("click", () => {
+        buttonMenu.classList.toggle("is-active");
+        mobile.classList.toggle("mobile");
     });
 
-    buttonMenu.addEventListener("click", () => {
-        buttonMenu.classList.toggle("is-active");
-        navCover.classList.toggle("mobile");
-    });
